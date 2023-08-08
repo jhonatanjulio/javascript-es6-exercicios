4c90e37a6d94e2a8302a7690c8cfe57fe489fd9d
jhonatanjulio <123335567+jhonatanjulio@users.noreply.github.com>
2023-08-08 10:27:00 -0300

    correção capitulo arrow functions

---
+++ Capítulo 10 - Arrow Functions/ex04 - Vou lavar sua boca com sabão!/ex04.js
@@ -0,0 +1,12 @@
+// Utilize as arrow funcions para deixar o código a seguir ainda menor.
+
+let palavroes = [
+    "Inconstitucionalíssimo",
+    "Otorrinolaringologista",
+    "Pneumoultramicroscopicossilicovulcanoconiose",
+    "Oftalmotorrinolaringologista"
+];
+
+let tamanhos =  palavroes.map(palavrao => palavrao.length);
+
+console.log(tamanhos); // [ 22, 22, 44, 28 ]
